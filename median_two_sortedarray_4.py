7e99a7cb5042b7e84a580d5f9c26fb9a9bfb1bab
goonhuyoon <goonhu.yoon18@imperial.ac.uk>
2020-05-18 08:57:43 +0900

    leetcode top 100 liked #4 question

---
+++ median_two_sortedarray_4.py
@@ -0,0 +1,11 @@
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        list_total = nums1 + nums2
+        list_total.sort()
+
+        if len(list_total) % 2 != 0:
+            return float(list_total[len(list_total )//2])
+        else:
+            return (list_total[len(list_total )//2] + list_total[len(list_total )/ / 2 -1] ) /2
