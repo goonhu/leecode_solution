febe384f81729fc033cdeb538cd2dcda9e26c412
goonhuyoon <goonhu.yoon18@imperial.ac.uk>
2020-05-18 01:25:06 +0900

    two sum leetcode 1 commit

--- two.py
+++ two.py
@@ -1,7 +1,14 @@
+class Solution:
+
+    def twoSum(self, nums, target):
+        hash_ = {}
+        for index, element in enumerate(nums):
+            n = target - element
+            if n not in hash_:
+                hash_[element] = index
+            else:
+                return [hash_[n], index]
 
 
-print("Asdafs")
 
-def leetcode(self, as):
 
-    return
